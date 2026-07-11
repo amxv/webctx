93f7f91350666ca866ce8dc0f66a5ce6f53539ab
amxv <a@ashray.xyz>
2026-07-12 02:40:07 +0530

    Update ZueDocs to 0.1.23
    (cherry picked from commit bc16cbdbd5b66597818b7b45928e03b73f2580ad)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
+import zueDocs from "zuedocs/astro";
 
 export default defineConfig({
-  output: "static"
+  output: "static",
+  integrations: [zueDocs()]
 });
